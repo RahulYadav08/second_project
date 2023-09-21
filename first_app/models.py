2247ac73b75d7361a5815a8644586d082cbc513b
RahulYadav08 <rkrrkumar511@gmail.com>
2023-09-21 16:59:01 +0530

    Member model added

--- first_app/models.py
+++ first_app/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Member(models.Model):
+    name = models.CharField(max_length=30)
+    age = models.IntegerField()
+
+    class Meta:
+        verbose_name_plural = "Member's"
+    def __str__(self):
+        return self.name
+    
